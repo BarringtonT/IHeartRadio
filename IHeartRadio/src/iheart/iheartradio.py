65d6d4ca486128b013692c444337db490258151c
Barrington W. Thompson <BarringtonThom@gmail.com>
2014-09-03 11:28:53 -0400

    second commit

--- IHeartRadio/src/iheart/iheartradio.py
+++ IHeartRadio/src/iheart/iheartradio.py
@@ -4,3 +4,11 @@ Created on Sep 3, 2014
 
 @author: boss
 '''
+
+from urllib.error import URLError, HTTPError
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+
+
+url = input('Please enter web address')
+req = Request(url)
